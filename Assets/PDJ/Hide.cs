c7070efc4edab97d03afcb44aa6717ae40d6c614
Vicenzo <Vicenzux@gmail.com>
2026-03-30 06:26:23 -0300

    trabalho inteiro de assets

--- Assets/PDJ/Hide.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Hide : MonoBehaviour
-{
-    public void Click()
-    {
-        gameObject.SetActive(!gameObject.activeSelf);
-    }
-}
--- Assets/PDJ/Paint.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Paint : MonoBehaviour
-{
-    void OnMouseOver()
-    {
-        GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
-    }
-}
